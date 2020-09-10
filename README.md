7b24a2d447d7f17935ab5493fb9e65371c6adfb0
Senthilkannan <51955692+techkanna@users.noreply.github.com>
2020-09-10 19:02:56 +0530

    :rocket: builded

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## Expense Tracker
 
+for live [demo](https://techkanna.github.io/vtr-expense-tracker/)
 Keep track of income and expenses. Add and remove items and save to local storage
 
 ## Project Specifications
---
+++ docs/precache-manifest.d9bb25083a5992fbb72dd807a49a72f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff0cce14b0db35acd76d52bdbe520f54",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5436e2ff98c8664e7a24",
+    "url": "./static/css/main.2f6873f6.chunk.css"
+  },
+  {
+    "revision": "f2cf3833cb58d4f57016",
+    "url": "./static/js/2.c57cdb69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c57cdb69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5436e2ff98c8664e7a24",
+    "url": "./static/js/main.e136dfd4.chunk.js"
+  },
+  {
+    "revision": "46f00c63568a0040936a",
+    "url": "./static/js/runtime-main.bf6b2eda.js"
+  }
+]);
